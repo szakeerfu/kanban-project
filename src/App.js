583769b753d9a18168fd980a8faa572517dcc2ae
szakeerfu <shaikzakeer202557@gmail.com>
2023-06-05 19:50:18 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ function App() {
       <h1>
         new repo
         kjlkjlkj
-<<<<<<< HEAD
         zakeerhussian
         add merged
-=======
         hii ashish
         hii zakir
->>>>>>> 485f3975f0583a373a685d6ac1620bb2884fb7ac
+        second add
       </h1>
 
     </div>
