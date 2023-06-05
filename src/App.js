6add3209e7a33bab6d1e7e61057c677decbae180
Aniketoo7 <apotheosis788@gmail.com>
2023-06-06 00:00:37 +0530

    NavBar_added-By-Aniket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+import Navbar from './components/Navbar/navbar';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hii
-      <h1>
-        hii all teams members ahhhghjf
-      </h1>
-
+     <Navbar/>
     </div>
   );
 }
