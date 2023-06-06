c3594d5b1353c3acddc31093c19f4723a763e6a3
szakeerfu <shaikzakeer202557@gmail.com>
2023-06-06 23:10:53 +0530

    zakeer add dragand drop functionally and pop up button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
+ 
 import Navbar from './components/Navbar/navbar';
-
 import KanbanBoard from './components/KanbanBoard';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <KanbanBoard />
-
-    </div>
+   
+      <div className="App">
+        <Navbar />
+        <DndProvider backend={HTML5Backend}>
+         
+            <KanbanBoard />  
+       
+        
+        </DndProvider>
+      </div>
+   
   );
-}
+};
 
 export default App;
