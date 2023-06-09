47d7b8f0da2715c495ff1a435f97103bd21691c1
gurank789 <gurank7890verma@gmail.com>
2023-06-09 12:23:04 +0530

    newbranchadded

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Board from "./components/Board/Board";
 
 import "./App.css";
 import Editable from "./components/Editabled/Editable";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [boards, setBoards] = useState(
@@ -123,10 +124,11 @@ function App() {
   return (
     <div className="app">
       <div className="app_nav">
-        
+      <Navbar />
       </div>
       <div className="app_boards_container">
         <div className="app_boards">
+          
           {boards.map((item) => (
             <Board
               key={item.id}
